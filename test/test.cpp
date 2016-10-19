0e07e587dad9f8375d0b81238d6302f7768d935e
Nickel <jianhuizhu@tuputech.com>
2016-10-19 17:33:22 +0800

    fixed syntax error in sample data line

--- test/test.cpp
+++ test/test.cpp
@@ -23,15 +23,15 @@ void printResult(int rc, long statusCode, const string & result);
 
 
 int main(int argc, char *argv[]) {
-    string secretId = "your_secret_id"
+    string secretId = "your_secret_id";
     Recognition *rec = new Recognition("Path-of-Your-PKCS8-Private-Key");
 
     //Set sub-user identifier for billing and statistics (optional feature)
     //rec->setUID("user-bucket-xyz");
 
-    string imgUrl = "http://www.yourdomain.com/img/1.jpg"
-    string imgPath1 = "/home/user/img/1.jpg"
-    string imgPath2 = "/home/user/img/2.jpg"
+    string imgUrl = "http://www.yourdomain.com/img/1.jpg";
+    string imgPath1 = "/home/user/img/1.jpg";
+    string imgPath2 = "/home/user/img/2.jpg";
 
     vector<string> images1 = { imgUrl };
     vector<string> images2 = { imgPath1, imgPath2 };
