8b142237db151fd74185f3d154affa0bd4aac8fc
pw <pw@example.com>
2022-05-16 20:41:40 +0800

    Added support for sequences: performWithURLSequence & performWithPathSequence

--- include/Recognition.hpp
+++ include/Recognition.hpp
@@ -71,13 +71,13 @@ class Recognition
         int performWithURLSequence(const std::string & secretId,
             std::string & result, long *statusCode,
             const std::vector<std::string> & imageURLs,
-            const std::vector<std::string> & tags = std::vector<std::string>()
+            const std::vector<std::string> & tags = std::vector<std::string>(),
             const std::vector<std::string> & sequenceIds = std::vector<std::string>() );
 
         int performWithPathSequence(const std::string & secretId,
             std::string & result, long *statusCode,
             const std::vector<std::string> & imagePaths,
-            const std::vector<std::string> & tags = std::vector<std::string>()
+            const std::vector<std::string> & tags = std::vector<std::string>(),
             const std::vector<std::string> & sequenceIds = std::vector<std::string>() );
 
         //Don't mix the use of URL and path/binary
